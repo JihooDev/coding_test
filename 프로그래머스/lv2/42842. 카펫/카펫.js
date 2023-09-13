9d66b36a3348416e5e1cdc6d5977127dccb033f4
김지후 <105398995+JihooDev@users.noreply.github.com>
2023-09-14 08:10:49 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -0,0 +1,8 @@
+function solution(brown, yellow) {
+  for (let i = 3; i <= (brown+yellow)/i; i++) {
+      let width = Math.floor((brown+yellow)/i);
+      if( (width-2)*(i-2)=== yellow) {
+          return [width,i];
+      }
+  }
+}
