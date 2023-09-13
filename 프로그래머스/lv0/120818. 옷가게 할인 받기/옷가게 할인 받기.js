7b02baebf8381a613ed2c8558196120119bcdff9
김지후 <105398995+JihooDev@users.noreply.github.com>
2023-09-13 09:00:32 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,11 @@
+function solution(price) {
+ if (price >= 100000 && price < 300000) {
+        return Math.floor(price * .95)
+    } else if (price >= 300000 && price < 500000) {
+        return Math.floor(price * .9)
+    } else if (price >= 500000) {
+        return Math.floor(price * .8)
+    } else {
+        return price
+    }
+}
