2b721dcae9021b3ceffe50757786508ee272b13f
김지후 <105398995+JihooDev@users.noreply.github.com>
2023-10-05 12:31:33 +0900

    [level 1] Title: 삼총사, Time: 0.41 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -0,0 +1,18 @@
+function solution(number) {
+  // 결과 값 (삼총사 숫자)
+  let result = 0;
+
+  // 재귀 함수
+  const combination = (current, start) => {
+      if (current.length === 3) {
+          result += current.reduce((acc, cur) => acc + cur, 0) === 0 ? 1 : 0;
+          return;
+      }
+
+      for (let i = start; i < number.length; i++) {
+          combination([...current, number[i]], i + 1);
+      }
+  }
+  combination([], 0);
+  return result;
+}
