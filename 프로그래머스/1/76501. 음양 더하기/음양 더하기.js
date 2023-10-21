1f8173bae98d7f66d78a866ced6b3f26fd50f2f0
김지후 <105398995+JihooDev@users.noreply.github.com>
2023-10-21 09:49:19 +0900

    [level 1] Title: 음양 더하기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,11 @@
+function solution(absolutes, signs) {
+  let result = 0;
+
+  for(let i = 0; i < absolutes.length; i++) {
+    if(!signs[i]) absolutes[i] = absolutes[i] * -1;
+
+    result+=absolutes[i];
+  }
+
+  return result;
+}
