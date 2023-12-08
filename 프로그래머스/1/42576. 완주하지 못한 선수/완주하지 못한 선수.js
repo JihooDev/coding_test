b1253da372b7fbd46fae10e0f78c3fd44105ed08
김지후 <105398995+JihooDev@users.noreply.github.com>
2023-12-08 09:54:41 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 32.74 ms, Memory: 67 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,10 +1,31 @@
-function solution(participant, completion) {
-  const sortParticipant = participant.sort();
-  const sortCompletion = completion.sort();
+// function solution(participant, completion) {
+//   const sortParticipant = participant.sort();
+//   const sortCompletion = completion.sort();
 
-  for(let i = 0; i <= sortParticipant.length; i++) {
-    if(sortParticipant[i] !== sortCompletion[i]) {
-      return sortParticipant[i]
+//   for(let i = 0; i <= sortParticipant.length; i++) {
+//     if(sortParticipant[i] !== sortCompletion[i]) {
+//       return sortParticipant[i]
+//     }
+//   } 
+// }
+
+function solution(participant, completion) {
+  const participantMap = new Map();
+
+  participant.forEach((value) => {
+    if (participantMap.has(value)) {
+      participantMap.set(`${value}`, participantMap.get(`${value}`) + 1);
+    } else {
+      participantMap.set(value, 1);
     }
-  } 
+  });
+
+  completion.forEach((value) => {
+    participantMap.set(`${value}`, participantMap.get(`${value}`) - 1);
+    if (participantMap.get(`${value}`) === 0) {
+      participantMap.delete(`${value}`);
+    }
+  });
+
+  return Array.from(participantMap)[0][0];
 }
