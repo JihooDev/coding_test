b691ec45dc6328480c2d97dd2ca5331c7a3a2e67
김지후 <105398995+JihooDev@users.noreply.github.com>
2023-12-18 12:39:18 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 48.79 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,19 @@
+// function solution(arr) {
+//     return arr.filter((item, idx) => item !== arr[idx + 1]);
+// }
+
+function solution(arr) {
+    let stack = [];
+
+    arr.forEach(nums => {
+        if (stack.length === 0) {
+            stack.push(nums);
+        }
+
+        if (stack[stack.length - 1] !== nums) {
+            stack.push(nums);
+        }
+    })
+
+    return stack;
+}
