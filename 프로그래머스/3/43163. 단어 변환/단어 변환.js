ca9228b72767436f47cacddfe5dd178c5eeccae0
김지후 <105398995+JihooDev@users.noreply.github.com>
2024-01-29 09:25:11 +0900

    [level 3] Title: 단어 변환, Time: 2.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -0,0 +1,33 @@
+function solution(begin, target, words) {
+    const visited = { [begin]: 0 };
+    const queue = [begin];
+
+    while (queue.length) {
+        const current = queue.shift();
+
+        if (current === target) break;
+
+        words.forEach(value => {
+            console.log(value, current);
+            if (isConnected(value, current) && !visited[value]) {
+                visited[value] = visited[current] + 1;
+                queue.push(value)
+            }
+        })
+    }
+
+    const result = visited[target] ?? 0;
+
+    return result;
+}
+
+const isConnected = (str1, str2) => {
+    let count = 0;
+    const len = str1.length;
+
+    for (let i = 0; i < len; i++) {
+        if (str1[i] !== str2[i]) count++;
+    }
+
+    return count === 1 ? true : false;
+}
