75e52eb63a4cd36172f4c6466b3755efc8783a01
김지후 <105398995+JihooDev@users.noreply.github.com>
2024-04-13 23:30:56 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 58.72 ms, Memory: 67.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,31 +1,25 @@
-// function solution(participant, completion) {
-//   const sortParticipant = participant.sort();
-//   const sortCompletion = completion.sort();
-
-//   for(let i = 0; i <= sortParticipant.length; i++) {
-//     if(sortParticipant[i] !== sortCompletion[i]) {
-//       return sortParticipant[i]
-//     }
-//   } 
-// }
-
 function solution(participant, completion) {
-  const participantMap = new Map();
+    const participantMap = new Map();
 
-  participant.forEach((value) => {
-    if (participantMap.has(value)) {
-      participantMap.set(`${value}`, participantMap.get(`${value}`) + 1);
-    } else {
-      participantMap.set(value, 1);
+    for (let i = 0; i < participant.length; i++) {
+        if (participantMap.has(participant[i])) {
+            participantMap.set(participant[i], participantMap.get(participant[i]) + 1);
+        } else {
+            participantMap.set(participant[i], 1);
+        }
     }
-  });
 
-  completion.forEach((value) => {
-    participantMap.set(`${value}`, participantMap.get(`${value}`) - 1);
-    if (participantMap.get(`${value}`) === 0) {
-      participantMap.delete(`${value}`);
+    for (let i = 0; i < completion.length; i++) {
+        if (participantMap.has(completion[i])) {
+            participantMap.set(completion[i], participantMap.get(completion[i]) - 1)
+
+            if (participantMap.get(completion[i]) === 0) {
+                participantMap.delete(completion[i]);
+            }
+        };
     }
-  });
 
-  return Array.from(participantMap)[0][0];
+    const player = Array.from(participantMap)[0][0];
+
+    return player;
 }
