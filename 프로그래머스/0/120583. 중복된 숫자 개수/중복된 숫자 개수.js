b0ca98c351d633d56153dbd34ad89e3666d78826
김지후 <105398995+JihooDev@users.noreply.github.com>
2024-07-13 10:31:11 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -0,0 +1,11 @@
+function solution(array, n) {
+    let result = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === n) {
+            result++
+        }
+    }
+
+    return result;
+}
