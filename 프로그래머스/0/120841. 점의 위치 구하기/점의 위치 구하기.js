25f94833be80c00ec000e79c6afbb71f2ebda5ed
김지후 <105398995+JihooDev@users.noreply.github.com>
2024-07-13 10:29:02 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -0,0 +1,8 @@
+function solution(dot) {
+    const [x, y] = dot;
+
+    if (x >= 0 && y >= 0) return 1;
+    if (x < 0 && y >= 0) return 2;
+    if (x < 0 && y < 0) return 3;
+    if (x >= 0 && y < 0) return 4;
+}
