a608b1873076d0b156c280a5796d1c8a0e1d85b9
김지후 <105398995+JihooDev@users.noreply.github.com>
2024-07-15 09:02:42 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -0,0 +1,13 @@
+function solution(num_list) {
+    let result = [0, 0];
+
+    for (let i = 0; i < num_list.length; i++) {
+        if (num_list[i] % 2 === 0) {
+            result[0]++;
+        } else {
+            result[1]++;
+        }
+    }
+
+    return result;
+}
